c84ad0ec0323d959a07a16803143428af1ff2da7
Yuliya Mironova <rabkevich15@gmail.com>
2022-07-06 16:35:49 +0200

    table withought readeble time

--- airoport.js
+++ airoport.js
@@ -2,7 +2,20 @@ console.log(flyghts);
 const tRows = document.getElementById("table");
 
 let oneGate = flyghts[0].gate;
-let all = flyghts.map((e) => `<tr><td>${e.gate}</td></tr>`).join("");
+let all = flyghts
+	.filter((e) => e.gate != undefined)
+	.map(
+		(e) => `<tr>
+  <td>${e.alname}</td>
+  <td>${e.fnr}</td>
+  <td>${e.apname}</td>
+  <td>${e.schedArr}</td>
+  <td>${e.schedArr}</td>
+  <td>${e.gate}</td>
+  <td>${e.status}</td>
+  </tr>`
+	)
+	.join("");
 
 console.log(all);
 
